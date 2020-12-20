6de57c6afde4aae4fb47d643fca71a47f1e81741
trails-game <admin@trails-game.com>
2020-12-20 12:53:36 -0500

    refactor some code

--- src/index.js
+++ src/index.js
@@ -5,20 +5,42 @@ import SpriteText from 'three-spritetext';
 
 export default class RelationChart {
 
-  constructor(myMap) {
-    const Graph = ForceGraph3D()(myMap)
-        .jsonUrl('https://raw.githubusercontent.com/vasturiano/3d-force-graph/master/example/datasets/miserables.json')
-        .nodeAutoColorBy('group')
-        .nodeThreeObject(node => {
-          const sprite = new SpriteText(node.id);
-          sprite.material.depthWrite = false; // make sprite background transparent
-          sprite.color = node.color;
-          sprite.textHeight = 8;
-          return sprite;
-        });
+  constructor(mapContainer, data) {
+    if (typeof(data) === 'string') {
+      this.Graph = ForceGraph3D()(mapContainer).jsonUrl(data);
+    } else {
+      this.Graph = ForceGraph3D()(mapContainer).graphData(data);
+    }
+  }
+
+  init () {
+    this.Graph.nodeAutoColorBy('group')
+      .nodeThreeObject(node => {
+        const sprite = new SpriteText(node.id);
+        sprite.material.depthWrite = false; // make sprite background transparent
+        sprite.color = node.color;
+        sprite.textHeight = 8;
+        return sprite;
+      })
+      .linkThreeObjectExtend(true)
+      .linkThreeObject(link => {
+        // extend link with text sprite
+        const sprite = new SpriteText(`${link.source} > ${link.target}`);
+        sprite.color = 'lightgrey';
+        sprite.textHeight = 1.5;
+        return sprite;
+      })
+      .linkPositionUpdate((sprite, { start, end }) => {
+        const middlePos = Object.assign(...['x', 'y', 'z'].map(c => ({
+          [c]: start[c] + (end[c] - start[c]) / 2 // calc middle point
+        })));
+
+        // Position sprite
+        Object.assign(sprite.position, middlePos);
+      });
 
     // Spread nodes a little wider
-    Graph.d3Force('charge').strength(-120);
+    this.Graph.d3Force('charge').strength(-120);
   }
 }
 
