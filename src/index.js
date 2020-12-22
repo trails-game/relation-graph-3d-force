377ed1e5a9a7be02dcb7c97c5f42264d0e10c3ac
trails-game <admin@trails-game.com>
2020-12-21 21:10:58 -0500

    various performance improvements and add particles

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ export default class RelationChart {
     if (typeof(data) === 'string') {
       this.Graph = ForceGraph3D()(mapContainer).jsonUrl(data);
     } else {
-      this.buildNeighbours(data);
+      this.buildNeighboursAndTestPos(data);
     }
 
     this.Graph = ForceGraph3D()(mapContainer).graphData(data);
@@ -26,12 +26,13 @@ export default class RelationChart {
     this.highlightNodes = new Set();
     this.highlightLinks = new Set();
     this.hoverNode = null;
+    this.clickedNode = null;
 
     this.nodes = [];
     this.links = [];
   }
 
-  buildNeighbours(data) {
+  buildNeighboursAndTestPos(data) {
     return data.links.forEach(link => {
       const a = data.nodes[link.source];
       const b = data.nodes[link.target];
@@ -44,23 +45,47 @@ export default class RelationChart {
       !b.links && (b.links = []);
       a.links.push(link);
       b.links.push(link);
+
+      link.textPos = (Math.floor(Math.random() * 5) + 4) / 10;
     });
   }
 
   updateHighlight() {
     // trigger update of highlighted objects in scene
+
     this.nodes.forEach(node => {
+      /*
+      if (this.hoverNode && this.highlightNodes.has(node)) {
+        node.__threeObj.material.opacity = 1;
+      } else {
+        node.__threeObj.material.opacity = 0.6;
+      }
+      */
       if (this.hoverNode && !this.highlightNodes.has(node)) {
         node.__threeObj.material.opacity = 0.1;
+      } else if (!this.hoverNode) {
+        node.__threeObj.material.opacity = 0.6;
       } else {
         node.__threeObj.material.opacity = 1;
       }
+
     });
 
+    this.links.forEach(link => {
+      if (this.hoverNode && !this.highlightLinks.has(link)) {
+        link.__lineObj.visible = false;
+      } else {
+        link.__lineObj.visible = true;
+      }
+
+    });
+    this.Graph.linkDirectionalParticles(this.Graph.linkDirectionalParticles());
+    /*
     this.Graph
       .linkWidth(this.Graph.linkWidth())
       //.nodeThreeObject(this.Graph.nodeThreeObject())
       .linkDirectionalParticles(this.Graph.linkDirectionalParticles());
+      */
   }
 
   init () {
@@ -83,14 +108,14 @@ export default class RelationChart {
         );
 
         circle.material.transparent = true;
-        if (this.hoverNode && !this.highlightNodes.has(node)) {
-          circle.material.opacity = 0.1;
-        }
+        circle.material.opacity = 0.6;
+
         return circle;
       })
       .onNodeHover(node => {
         // no state change
-        if ((!node && !this.highlightNodes.size) || (node && this.hoverNode === node)) return;
+        if ((!node && !this.highlightNodes.size) || (node && this.hoverNode === node) ||
+              this.clickedNode) return;
 
         this.highlightNodes.clear();
         this.highlightLinks.clear();
@@ -103,10 +128,43 @@ export default class RelationChart {
         this.hoverNode = node || null;
         this.updateHighlight();
       })
+      .onNodeClick(node => {
+        if (this.clickedNode === node) {
+          this.clickedNode = null;
+          this.hoverNode = null;
+          this.highlightNodes.clear();
+          this.highlightLinks.clear();
+          this.updateHighlight();
+        } else if (this.clickedNode) {
+          this.clickedNode = node;
+          this.hoverNode = node;
+          this.highlightNodes.clear();
+          this.highlightLinks.clear();
+          this.highlightNodes.add(node);
+          node.neighbors.forEach(neighbor => this.highlightNodes.add(neighbor));
+          node.links.forEach(link => this.highlightLinks.add(link));
+          this.updateHighlight();
+        } else {
+          this.clickedNode = node;
+        }
+      })
+      .onNodeRightClick(node => {
+        let win = window.open("https://trails-game.com/character/juna-crawford/", '_blank');
+        win.focus();
+      })
+      .onBackgroundClick(() => {
+        this.highlightNodes.clear();
+        this.highlightLinks.clear();
+        this.clickedNode = null;
+        this.hoverNode = null;
+        this.updateHighlight();
+      })
+      .linkDirectionalParticles(link => this.highlightLinks.has(link) ? 3 : 0)
+      .linkDirectionalParticleWidth(0.5)
       .linkThreeObjectExtend(true)
       .linkThreeObject(link => {
         this.links.push(link);
-        console.log(link);
+        //console.log(link);
         // extend link with text sprite
         const sprite = new SpriteText(link.relation);
         sprite.color = 'lightgrey';
@@ -114,7 +172,9 @@ export default class RelationChart {
         return sprite;
       })
       .linkCurvature(0.2)
-      .linkWidth(link => this.highlightLinks.has(link) ? 1 : 0)
+      //.linkWidth(0.2)
+      //.linkWidth(link => this.highlightLinks.has(link) ? 1 : 0)
+      //.linkVisibility(link => this.highlightLinks.has(link) ? false : true)
       // eslint-disable-next-line no-unused-vars
       .linkPositionUpdate((sprite, { start, end }, link) => {
         /*
@@ -125,7 +185,7 @@ export default class RelationChart {
         // Position sprite
         // Object.assign(sprite.position, middlePos);
         // console.log(link.__curve.getPoint(0.5));
-        Object.assign(sprite.position, link.__curve.getPoint(0.5));
+        Object.assign(sprite.position, link.__curve.getPoint(link.textPos));
       });
 
     // Spread nodes a little wider
